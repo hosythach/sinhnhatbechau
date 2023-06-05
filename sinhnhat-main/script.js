fa87d4f3c6fb0904080b6afa1e3eca72b3f35221
ThachHS <luusa2k1@gmail.com>
2023-06-06 02:37:42 +0700

    6623

---
+++ sinhnhat-main/script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "MINH CHAU" && b=="06062004")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
